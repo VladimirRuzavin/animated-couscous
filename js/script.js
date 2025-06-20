fe9dfd0ff85fdba59b0ac54d617966ca9a766800
Vladimir Ruzavin <vovchesk@yandex.ru>
2025-06-20 18:20:56 +0300

    подключил js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// let burger = document.querySelector(".burger");
+// console.log(burger)
+// burger.onclick = function() {
+//     alert('Спасибо');
+//   };
+let age = 23;
+let user = "Vovchan";
+let user2 = "Данилыч";
+let isStudent = true;
+let btn = document.querySelector(".header__reserv");
+btn.onclick = function () {
+  alert("Привет меня зовут " + user2 + " " + "мне " + age); 
+};
