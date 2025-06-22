c39899531604d24bb603af1f5e7c03e7552c4b1e
Vladimir Ruzavin <vovchesk@yandex.ru>
2025-06-22 15:47:40 +0300

    создал переменную и присвоил ей значение

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@
 // burger.onclick = function() {
 //     alert('Спасибо');
 //   };
-let age = 23;
-let user = "Vovchan";
-let user2 = "Данилыч";
-let isStudent = true;
-let btn = document.querySelector(".header__reserv");
+// let age = 23;
+// let user = "Vovchan";
+// let user2 = "Данилыч";
+// let isStudent = true;
+// let btn = document.querySelector(".header__reserv");
+// btn.onclick = function () {
+//    alert("Привет меня зовут " + user2 + " " + "мне " + age);
+// };
+let city = "Cheboksary";
+let btn = document.querySelector(".advantages__button");
 btn.onclick = function () {
-  alert("Привет меня зовут " + user2 + " " + "мне " + age); 
-};
+    alert(city);
+ };
+
