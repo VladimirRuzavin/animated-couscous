280ff0af5f0d6e3bf041a66133186306de4f89ff
Vladimir Ruzavin <vovchesk@yandex.ru>
2025-06-23 19:31:12 +0300

    создал переменную

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// let burger = document.querySelector(".burger");
-// console.log(burger)
-// burger.onclick = function() {
-//     alert('Спасибо');
-//   };
+//  let burger = document.querySelector(".burger");
+//  console.log(burger)
+//  burger.onclick = function() {
+//      alert('Спасибо');
+//    };
 // let age = 23;
 // let user = "Vovchan";
 // let user2 = "Данилыч";
@@ -11,9 +11,20 @@
 // btn.onclick = function () {
 //    alert("Привет меня зовут " + user2 + " " + "мне " + age);
 // };
-let city = "Cheboksary";
-let btn = document.querySelector(".advantages__button");
-btn.onclick = function () {
-    alert(city);
+ let city = "Cheboksary";
+ city = "Moscow";
+ let btn = document.querySelector(".advantages__button");
+ console.log(btn);
+ btn.onclick = function () {
+   alert(city);
  };
+
+ const animal = "cat";
+ const game = "fifa";
+ let btn = document.querySelector(".header__reserv");
+ console.log(btn);
+ btn.onclick = function () {
+   alert(game);
+ };
+
 
