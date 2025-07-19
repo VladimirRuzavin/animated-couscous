129cf604236ee767ec95d096008e8a94f192bd5b
Vladimir Ruzavin <vovchesk@yandex.ru>
2025-07-19 16:40:32 +0300

    функции

--- js/script-new.js
+++ js/script-new.js
@@ -13,24 +13,40 @@ function clickOnBurger() {
 }
 clickOnBurger();
 
+
+//задание 1
 function greetings() {
   alert("sayHello");
 }
 
 greetings();
 
+//задание 2
 function sum() {
  let x = 5 , y = 10;
  alert(x + y);
 }
 
-sum() ;
+sum();
 
+
+//задание 3
 let calcSquare = (a , b) =>{
   let result = (a + b) ** 2;
   return result;
 }
 
-alert (calcSquare (15, 15) )
+alert (calcSquare (15, 15) );
+
+
+//задание 4
+function calc() {
+  let double = 15 * 2;
+  alert (double);
+}
+
+calc();
+
+
 
 
