be29587bbea7c7835a61a61e7996f4dc4ace7868
Vladimir Ruzavin <vovchesk@yandex.ru>
2025-09-03 15:26:03 +0300

    сделал бургер

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,10 @@
 let burger = document.querySelector(".burger"); 
 let burgerMenu = document.querySelector(".burger__menu");
 console.log(burger); 
-let showMessage = function () {
-  burgerMenu.style.display = 'block';
-};
-burger.addEventListener("click", showMessage);
+;
 
 let hideMessage = function () {
- burger.classList.toggle('burger__menu') ;
+ burgerMenu.classList.toggle('visible', ) ;
 };
 burger.addEventListener("click", hideMessage);
-
 
