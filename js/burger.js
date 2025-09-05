f4a2e4a819170110a456545d505fc13bf9c690e3
Vladimir Ruzavin <vovchesk@yandex.ru>
2025-09-05 16:37:50 +0300

    верстка бургера

--- js/burger.js
+++ js/burger.js
@@ -1,10 +1,15 @@
 let burger = document.querySelector(".burger"); 
 let burgerMenu = document.querySelector(".burger__menu");
 console.log(burger); 
+let burgerLogo = document.querySelector(".burger__logo");
 ;
 
 let hideMessage = function () {
- burgerMenu.classList.toggle('visible', ) ;
+ burgerMenu.classList.toggle('visible') ;
 };
 burger.addEventListener("click", hideMessage);
 
+let showMessage = function () {
+ burgerLogo.classList.toggle('invisible') ;
+};
+burgerLogo.addEventListener("click", showMessage);
